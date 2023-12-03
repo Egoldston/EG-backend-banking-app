7d0cdfdf8c606542c9bcb908a25cbe238394f510
Elizabeth Goldston <124409977+Egoldston@users.noreply.github.com>
2023-12-03 15:18:02 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,36 +9,36 @@ const e = require('express');
 app.use(express.static('public'));
 // app.use(cors({origin: true, credentials: true }));
 
-// app.use(cors({
-//     origin: 'https://bb-backend-py4z.onrender.com/',
-//     headers: ["Content-Type"],
-//     credentials: true,
-// }
-// ));
-// app.options('*', cors());
+app.use(cors({
+    origin: 'https://bb-backend-py4z.onrender.com/',
+    headers: ["Content-Type"],
+    credentials: true,
+}
+));
+app.options('*', cors());
 
 // app.use(cors({ origin: ['http://localhost:3000', 'https://bb-backend-py4z.onrender.com/'], credentials: true }))
 
-app.use((req, res, next) => {
-    res.setHeader(
-      "Access-Control-Allow-Origin",
-      "https://bb-backend-py4z.onrender.com/"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-    );
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    res.setHeader("Access-Control-Allow-Private-Network", true);
-    //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-    res.setHeader("Access-Control-Max-Age", 7200);
+// app.use((req, res, next) => {
+//     res.setHeader(
+//       "Access-Control-Allow-Origin",
+//       "https://bb-backend-py4z.onrender.com/"
+//     );
+//     res.setHeader(
+//       "Access-Control-Allow-Methods",
+//       "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+//     );
+//     res.setHeader(
+//       "Access-Control-Allow-Headers",
+//       "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+//     );
+//     res.setHeader("Access-Control-Allow-Credentials", true);
+//     res.setHeader("Access-Control-Allow-Private-Network", true);
+//     //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+//     res.setHeader("Access-Control-Max-Age", 7200);
   
-    next();
-  });
+//     next();
+//   });
 
 
 
