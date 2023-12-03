022c00cc2b477c2a5f4dbdd0b7c9b519ae8d4dab
Elizabeth Goldston <124409977+Egoldston@users.noreply.github.com>
2023-12-03 13:59:11 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,8 +7,19 @@ const e = require('express');
 
 // used to serve static files from public directory
 app.use(express.static('public'));
-app.use(cors({credentials: true, origin: 'https://bb-backend-py4z.onrender.com/'}));
-
+// app.use(cors({credentials: true, origin: 'https://bb-backend-py4z.onrender.com/'}));
+app.use(function(req, res, next) {
+    // res.header("Access-Control-Allow-Origin", "*");
+    const allowedOrigins = ['http://localhost:3000', 'https://bb-backend-py4z.onrender.com/'];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+         res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+    next();
+  });
 
 
 // Authentication Middleware
